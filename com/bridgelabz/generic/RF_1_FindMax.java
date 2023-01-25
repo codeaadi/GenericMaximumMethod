d1a4212acc15f9eeb6abfec83ec25c322eec88b7
codeaadi <adityadubey362@gmail.com>
2023-01-25 15:22:53 +0530

    Refactored: method to find maximum

---
+++ com/bridgelabz/generic/RF_1_FindMax.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.generic;
+
+import java.util.Scanner;
+
+        public class RF_1_FindMax {
+
+            public static <T extends Comparable<T>> void Maximum(T value1, T value2, T value3) {
+                T max = value1;
+                if(value1.compareTo(max)>0 && value2.compareTo(value3)>0)
+                    System.out.println(value2+" is Maximum");
+                else if(value3.compareTo(max)>0)
+                    System.out.println(value3+" is Maximum");
+                else
+                    System.out.println(value1+" is Maximum");
+            }
+
+            public static void main(String[] args) {
+
+                Scanner scan = new Scanner(System.in);
+
+                System.out.print("Enter the First Number : ");
+                Integer value1 = scan.nextInt();
+
+                System.out.print("Enter the Second Number : ");
+                Integer value2 = scan.nextInt();
+
+                System.out.print("Enter the Third Number : ");
+                Integer value3 = scan.nextInt();
+
+                Maximum(value1, value2, value3);
+            }
+        }
